db7cdece6fc7cfad83a5205cbeb41bf9cf721fcb
shrinidhi <chinnu.katti@gmail.com>
2021-07-27 17:02:03 +0530

    conduit clone commit

--- api/src/main/java/io/realworld/api/models/ArticlesResponse.kt
+++ api/src/main/java/io/realworld/api/models/ArticlesResponse.kt
@@ -7,7 +7,7 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class ArticlesResponse(
     @Json(name = "articles")
-    val articles: List<ArticleX>,
+    val articles: List<Article>,
     @Json(name = "articlesCount")
     val articlesCount: Int
 )
